638aa4f8151375985d277723e19a8479718696c2
Jhimys25 <94380843+JhimyR@users.noreply.github.com>
2021-12-11 15:42:18 -0500

    Update netsize.sh
    Añadi ultimos comandos

--- Tara 1.0/netsize.sh
+++ Tara 1.0/netsize.sh
@@ -1,8 +1,9 @@
 # Primero abrimos la ruta donde va ubicada la cerpeta de nuestro interes la carpeta de Saavedra2013 en la compitadora y depues:
 cd Saavedra2013
-#Seguidamente ejecutamos el comando:
+# Seguidamente ejecutamos el comando:
 cat n1.txt
 Ahora con cd ../../ esto me permitira regresar al repositorio incial o la carpeta que decees modificar
 r y p son los archivos donde vamos a iniciar nuestro comando for y despues especificando la carpeta que necesitamos con 
 cd ../../
 for r in ../Saavedra2013/*.txt; do cat $r | wc -l; done 
+for p in ../Saavedra2013/*.txt; do head -n 1 $p | tr -d " " | tr -d "\n" | wc -c: done

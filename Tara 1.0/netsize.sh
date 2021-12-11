ac872b24545425eaa3579cb8a0c1740916c55f99
Jhimys25 <94380843+JhimyR@users.noreply.github.com>
2021-12-11 15:11:49 -0500

    Update netsize.sh
    Añadi un comando para regresar al repositorio y poder modificar la información

--- Tara 1.0/netsize.sh
+++ Tara 1.0/netsize.sh
@@ -3,3 +3,5 @@ cd Saavedra2013
 #Seguidamente ejecutamos el comando:
 cat n1.txt
 Ahora con cd ../../ esto me permitira regresar al repositorio incial o la carpeta que decees modificar
+r y p son los archivos donde vamos a iniciar nuestro comando for y despues especificando la carpeta que necesitamos con 
+cd ../../

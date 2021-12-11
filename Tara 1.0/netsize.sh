d6e7885c594d12f54ad04cc0674366f64359b071
Jhimys25 <94380843+JhimyR@users.noreply.github.com>
2021-12-11 16:24:34 -0500

    Update netsize.sh
    Elimine algunos errores y enumere los pasos que debo seguir para poder realizar la pregunta

--- Tara 1.0/netsize.sh
+++ Tara 1.0/netsize.sh
@@ -1,5 +1,8 @@
-# Primero abrimos la ruta donde va ubicada la cerpeta de nuestro interes la carpeta de Saavedra2013 en la compitadora y depues:
-cd Saavedra2013
-# Seguidamente ejecutamos el comando:
+#PARA PODER REALIZAR EL EJERCICIO
+ 1.- Necesitamos ubicarnos en el Git bash, de preferencia el programa que mas facil te resulte.
+ 2.- Necesitamos ubicar la ruta:
+     cd Documents/Ikiam_2021data/CSB/unix/Saavedra2013
+#Despues de ubicar el directorio y la ruta.
+Escribiumos en el terminal
 cat n1.txt
-
+#Ya despues de imprimir todo lo que contenia Saavedra2013

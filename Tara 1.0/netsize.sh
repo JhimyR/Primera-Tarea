710f99cf218aee259c4a2e8920843cd0ab3561e3
Jhimys25 <94380843+JhimyR@users.noreply.github.com>
2021-12-11 16:28:51 -0500

    Update netsize.sh
    Añadi el eco para poder verificar el numero de filas y columnas

--- Tara 1.0/netsize.sh
+++ Tara 1.0/netsize.sh
@@ -3,6 +3,9 @@
  2.- Necesitamos ubicar la ruta:
      cd Documents/Ikiam_2021data/CSB/unix/Saavedra2013
 #Despues de ubicar el directorio y la ruta.
-Escribiumos en el terminal
+Escribimos en el terminal
 cat n1.txt
 #Ya despues de imprimir todo lo que contenia Saavedra2013
+ echo "El ńumero de columnas es:" ; head -n1 n1.txt | grep -o " " | wc -l ; echo "El numero de filas es:" ; wc -l n1.txt; echo "Fin de la ejecucion"
+
+ 

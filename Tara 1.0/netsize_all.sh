23da3679f31251681832cbe5b8ba62a75e6d1613
Jhimys25 <94380843+JhimyR@users.noreply.github.com>
2021-12-11 17:53:26 -0500

    Eleccion de la carpeta
    Corremos el Saavedra2013

--- Tara 1.0/netsize_all.sh
+++ Tara 1.0/netsize_all.sh
@@ -1,5 +1,7 @@
 #2.Ejercicio 1.10.3 [Advanced] Part 2
  Esta parte del ejercicio esta especificado en el README.md
  - Ahora elegimos las misma ruta del repositorio anterior
-  
+ Corremos la información con el siguiente 
+ cd Saavedra2013
+ 
  

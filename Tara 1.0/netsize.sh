6085201e2d8edad1bab55114ab0fadfe72be9f7a
Jhimys25 <94380843+JhimyR@users.noreply.github.com>
2021-12-11 15:07:35 -0500

    Update netsize.sh
    Añadi segundo comando para encontrar la ruta

--- Tara 1.0/netsize.sh
+++ Tara 1.0/netsize.sh
@@ -1,2 +1,4 @@
 # Primero abrimos la ruta donde va ubicada la cerpeta de nuestro interes la carpeta de Saavedra2013 en la compitadora y depues:
 cd Saavedra2013
+#Seguidamente ejecutamos el comando:
+cat n1.txt

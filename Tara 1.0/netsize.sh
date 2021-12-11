c613a056629d091bf8fa4e0bb871df0484fd9617
Jhimys25 <94380843+JhimyR@users.noreply.github.com>
2021-12-11 16:57:11 -0500

    Modificacion del netsize.sh
    Corrección ortográfica y gramatical

--- Tara 1.0/netsize.sh
+++ Tara 1.0/netsize.sh
@@ -2,16 +2,16 @@
  1.- Necesitamos ubicarnos en el Git bash, de preferencia el programa que mas facil te resulte.
  2.- Necesitamos ubicar la ruta:
      cd Documents/Ikiam_2021data/CSB/unix/Saavedra2013
-#Despues de ubicar el directorio y la ruta.
+#Después de ubicar el directorio y la ruta.
 Escribimos en el terminal
 cat n1.txt
-#Ya despues de imprimir todo lo que contenia Saavedra2013
+#Entonces se imprimió todo lo que contenia Saavedra2013
  echo "El ńumero de columnas es:" ; head -n1 n1.txt | grep -o " " | wc -l ; echo "El numero de filas es:" ; wc -l n1.txt; echo "Fin de la ejecucion"
 #Como resultado final obtenemos
 #El ńumero de columnas es:
 80
 #El numero de filas es:
 97 n1.txt
-#Fin de la ejecucion
+#Fin de la ejecución
 
  
